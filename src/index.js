a798944ba446032f4ce742c91cdd9f47a8346c86
Daniel Obare N <dnobare@gmail.com>
2022-07-01 19:50:14 +0300

    flatabeer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 // Code here
+
+function commWithServer() {
+    fetch('http://localhost:3000/beers/1')
+
+        .then(result => result.text())
+        .then(text => {
+            let main = document.getElementById('description')
+            main.innerHTML = text
+            console.log(text)
+        });
+}
+
+commWithServer()
+
+// function imageGoesHere() {
+//     fetch('http://localhost:3000/beers/1')
+
+//         .then(result => result.text())
+//         .then(img => {
+//             let img = document.getElementById('beer-image')
+//             img = "https://i.ibb.co/wQ4G0w1/flatiron-brew.png"
+//             return img
+//         });
+// }
+
+// imageGoesHere()
+
+// function yourReview() {
+//     let main = document.getElementById('review')
+//     main.innerHTML = text
+//     console.log(document.getElementById('your-review').innerHTML.text)
+// }
+// document.addEventListener('DOMContentLoaded', () => {
+//     let btn = document.getElementById('actualbutton');
+//     btn.addEventListener('click', () => {
+//         yourReview();
+//     })
+// })
+
+// yourReview()
+
+
+
